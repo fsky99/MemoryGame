41115acab6fb142cfd91888fda7625c8ddd86909
Fatima4 <fatima@gmail.com>
2024-01-18 13:21:35 +0300

    card flipping is working with a timer

--- Script.js
+++ Script.js
@@ -1,15 +1,133 @@
-let getID = document.querySelectorAll('th')
-console.log(getID)
+let icon1 = document.getElementById('icon1')
+let icon2 = document.getElementById('icon2')
+let icon3 = document.getElementById('icon3')
+let icon4 = document.getElementById('icon4')
+let icon5 = document.getElementById('icon5')
+let icon6 = document.getElementById('icon6')
+let icon8 = document.getElementById('icon8')
+let icon10 = document.getElementById('icon10')
 
-// addEventListener('click', () => {
-//   // console.log(getID)
-//   if (document.getElementById('icon1')) {
-//     console.log('icon is 1')
-//   } else if (document.getElementById('icon2')) {
-//     console.log('icon is 2')
-//   } else if (document.getElementById('icon3')) {
-//     console.log('icon is 3')
-//   } else if (document.getElementById('icon4')) {
-//     console.log('icon is 4')
-//   }
-// })
+let icon1Match = document.getElementById('icon1Match')
+let icon2Match = document.getElementById('icon2Match')
+let icon3Match = document.getElementById('icon3Match')
+let icon4Match = document.getElementById('icon4Match')
+let icon5Match = document.getElementById('icon5Match')
+let icon6Match = document.getElementById('icon6Match')
+let icon8Match = document.getElementById('icon8Match')
+let icon10Match = document.getElementById('icon10Match')
+
+let icon1IMG = document.getElementById('icon1IMG')
+let icon2IMG = document.getElementById('icon2IMG')
+let icon3IMG = document.getElementById('icon3IMG')
+let icon4IMG = document.getElementById('icon4IMG')
+let icon5IMG = document.getElementById('icon5IMG')
+let icon6IMG = document.getElementById('icon6IMG')
+let icon8IMG = document.getElementById('icon8IMG')
+let icon10IMG = document.getElementById('icon10IMG')
+
+let icon1MatchIMG = document.getElementById('icon1MatchIMG')
+let icon2MatchIMG = document.getElementById('icon2MatchIMG')
+let icon3MatchIMG = document.getElementById('icon3MatchIMG')
+let icon4MatchIMG = document.getElementById('icon4MatchIMG')
+let icon5MatchIMG = document.getElementById('icon5MatchIMG')
+let icon6MatchIMG = document.getElementById('icon6MatchIMG')
+let icon8MatchIMG = document.getElementById('icon8MatchIMG')
+let icon10MatchIMG = document.getElementById('icon10MatchIMG')
+
+icon1.addEventListener('click', () => {
+  icon1IMG.innerHTML = '<img src="Icons/icon1.png">'
+  setInterval(() => {
+    icon1IMG.innerHTML = ''
+  }, 1200)
+})
+icon2.addEventListener('click', () => {
+  icon2IMG.innerHTML = '<img src="Icons/icon2.png">'
+  setInterval(() => {
+    icon2IMG.innerHTML = ''
+  }, 1200)
+})
+icon3.addEventListener('click', () => {
+  icon3IMG.innerHTML = '<img src="Icons/icon3.png">'
+  setInterval(() => {
+    icon3IMG.innerHTML = ''
+  }, 1200)
+})
+icon4.addEventListener('click', () => {
+  icon4IMG.innerHTML = '<img src="Icons/icon4.png">'
+  setInterval(() => {
+    icon4IMG.innerHTML = ''
+  }, 1200)
+})
+icon5.addEventListener('click', () => {
+  icon5IMG.innerHTML = '<img src="Icons/icon5.png">'
+  setInterval(() => {
+    icon5IMG.innerHTML = ''
+  }, 1200)
+})
+icon6.addEventListener('click', () => {
+  icon6IMG.innerHTML = '<img src="Icons/icon6.png">'
+  setInterval(() => {
+    icon6IMG.innerHTML = ''
+  }, 1200)
+})
+icon8.addEventListener('click', () => {
+  icon8IMG.innerHTML = '<img src="Icons/icon8.png">'
+  setInterval(() => {
+    icon8IMG.innerHTML = ''
+  }, 1200)
+})
+icon10.addEventListener('click', () => {
+  icon10IMG.innerHTML = '<img src="Icons/icon10.png">'
+  setInterval(() => {
+    icon10IMG.innerHTML = ''
+  }, 1200)
+})
+
+icon1Match.addEventListener('click', () => {
+  icon1MatchIMG.innerHTML = '<img src="Icons/icon1.png">'
+  setInterval(() => {
+    icon1MatchIMG.innerHTML = ''
+  }, 1200)
+})
+icon2Match.addEventListener('click', () => {
+  icon2MatchIMG.innerHTML = '<img src="Icons/icon2.png">'
+  setInterval(() => {
+    icon2MatchIMG.innerHTML = ''
+  }, 1200)
+})
+icon3Match.addEventListener('click', () => {
+  icon3MatchIMG.innerHTML = '<img src="Icons/icon3.png">'
+  setInterval(() => {
+    icon3MatchIMG.innerHTML = ''
+  }, 1200)
+})
+icon4Match.addEventListener('click', () => {
+  icon4MatchIMG.innerHTML = '<img src="Icons/icon4.png">'
+  setInterval(() => {
+    icon4MatchIMG.innerHTML = ''
+  }, 1200)
+})
+icon5Match.addEventListener('click', () => {
+  icon5MatchIMG.innerHTML = '<img src="Icons/icon5.png">'
+  setInterval(() => {
+    icon5MatchIMG.innerHTML = ''
+  }, 1200)
+})
+icon6Match.addEventListener('click', () => {
+  icon6MatchIMG.innerHTML = '<img src="Icons/icon6.png">'
+  setInterval(() => {
+    icon6MatchIMG.innerHTML = ''
+  }, 1200)
+})
+icon8Match.addEventListener('click', () => {
+  icon8MatchIMG.innerHTML = '<img src="Icons/icon8.png">'
+  setInterval(() => {
+    icon8MatchIMG.innerHTML = ''
+  }, 1200)
+})
+icon10Match.addEventListener('click', () => {
+  icon10MatchIMG.innerHTML = '<img src="Icons/icon10.png">'
+  setInterval(() => {
+    icon10MatchIMG.innerHTML = ''
+  }, 1200)
+})
