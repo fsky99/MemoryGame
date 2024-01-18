61c2ca31a600cbc2861a1c67a2c8235d6eadb74a
Fatima4 <fatima@gmail.com>
2024-01-18 11:29:38 +0300

    added the JS link to the second html page

---
+++ Script.js
@@ -0,0 +1,15 @@
+let getID = document.querySelectorAll('th')
+console.log(getID)
+
+// addEventListener('click', () => {
+//   // console.log(getID)
+//   if (document.getElementById('icon1')) {
+//     console.log('icon is 1')
+//   } else if (document.getElementById('icon2')) {
+//     console.log('icon is 2')
+//   } else if (document.getElementById('icon3')) {
+//     console.log('icon is 3')
+//   } else if (document.getElementById('icon4')) {
+//     console.log('icon is 4')
+//   }
+// })
